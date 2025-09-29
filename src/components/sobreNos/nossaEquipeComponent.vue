ac1460cc48dd59c16f99df5516a7d424e532eb84
saraSchulz <sara.ifc2024@gmail.com>
2025-09-29 15:03:53 -0300

    aplicação de animações

--- src/components/sobreNos/nossaEquipeComponent.vue
+++ src/components/sobreNos/nossaEquipeComponent.vue
@@ -57,10 +57,15 @@ function voltar() {
       <div class="lista" :class="{ ativo: membroSelecionado }">
         <h3>Membros:</h3>
         <ul>
-          <li v-for="m in membros" :key="m.nome" @click="selecionarMembro(m)"> {{ m.nome }} </li>
+          <li v-for="m in membros" :key="m.nome" @click="selecionarMembro(m)"
+            :class="{ selecionado: membroSelecionado?.nome === m.nome }">
+            {{ m.nome }}
+            <span v-if="membroSelecionado?.nome === m.nome" class="mdi mdi-menu-right"></span>
+          </li>
         </ul>
       </div> <!-- Detalhes -->
-      <div class="detalhes" v-if="membroSelecionado"> <button @click="voltar">← Voltar</button>
+      <div class="detalhes" v-if="membroSelecionado">
+        <button @click="voltar"> <i class="mdi mdi-menu-left"></i>Voltar</button>
         <p><img :src="membroSelecionado.foto" :alt="membroSelecionado.nome"></p>
         <h3>{{ membroSelecionado.nome }}</h3>
         <p><b>Email:</b> {{ membroSelecionado.email }}</p>
@@ -70,102 +75,129 @@ function voltar() {
     </div>
   </section>
 </template>
-<style
-  scoped>
-  .nossoEquipe {
+<style scoped>
+.nossoEquipe {
+  width: 100%;
+  align-items: center;
+
+  & .titulo {
+    margin: 2vw 0;
+    display: flex;
+    justify-content: center;
+    font-size: 50px;
+
+    & h2 {
+      color: #000000;
+      text-shadow: 0 4px 7px rgba(0, 0, 0, 0.8);
+    }
+  }
+
+  & .equipe {
+    min-height: 90vh;
     width: 100%;
+    background: url("/public/fundo.png") no-repeat center center/cover;
+    box-shadow: 0 0 75px rgba(0, 0, 0, 0.8);
+    display: flex;
     align-items: center;
+    gap: 5vw;
+    overflow: hidden;
 
-    & .titulo {
-      margin: 2vw 0;
-      display: flex;
-      justify-content: center;
-      font-size: 50px;
+    & .lista {
+      flex: 1;
+      transform: translateX(0);
+      transition: transform 1s ease;
+      text-align: center;
 
-      & h2 {
+
+      & h3 {
+        font-size: 50px;
         color: #000000;
         text-shadow: 0 4px 7px rgba(0, 0, 0, 0.8);
       }
-    }
 
-    & .equipe {
-      min-height: 90vh;
-      width: 100%;
-      background: url("/public/fundo.png") no-repeat center center/cover;
-      box-shadow: 0 0 75px rgba(0, 0, 0, 0.8);
-      display: flex;
-      align-items: center;
-      gap: 5vw;
-      overflow: hidden;
-
-      & .lista {
-        flex: 1;
-        transition: transform 2s ease;
+      & ul {
         display: flex;
         flex-direction: column;
-        justify-content: center;
+        text-align: center;
         align-items: center;
 
-        & img {
-          object-fit: cover;
-          box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);
-        }
-
-        & h3 {
-          font-size: 50px;
+        & li {
           color: #000000;
-        }
-
-        & ul {
-          & li {
-            color: #000000;
-            font-size: 30px;
-            margin: 15px;
-            text-align: center;
-            cursor: pointer;
-            padding: 5px 10px;
-            border-radius: 20px;
-          }
-
-          & li.hover {
-            background-color: #90E0EF;
-            padding: 5px 10px;
-            border-radius: 20px;
-          }
-        }
-      }
-
-      & .lista.ativo {
-        transform: translateX(0%);
-      }
-
-      & .detalhes {
-        flex: 1;
-        opacity: 1;
-        transition: opacity 0.5s ease;
-
-        & button {
-          background-color: transparent;
-          border: none;
-          font-size: 20px;
-          cursor: pointer;
-          margin-bottom: 20px;
-          text-align: center;
-        }
-
-        & h3 {
           font-size: 30px;
-          color: #000000;
-          margin-bottom: 20px;
+          margin: 15px;
           text-align: center;
+          cursor: pointer;
+          padding: 5px 70px;
+          border-radius: 50px;
+          transition: transform 0.3s ease, background-color 0.3s ease;
+
         }
 
-        & p {
-          color: #000;
-          font-size: 20px;
-          text-align: center;
+        & li:hover {
+          transform: scale(1.1);
+          background-color: rgba(144, 224, 239, 0.3);
         }
+
+        & li.selecionado {
+          transform: scale(1.15);
+          background-color: rgba(144, 224, 239, 0.3);
+
+        }
+
+        & li .mdi {
+          margin-left: 10px;
+
+          color: #000000;
+          vertical-align: middle;
+        }
+      }
+    }
+
+    & .lista.ativo {
+      transform: translateX(-10%);
+    }
+
+    & .detalhes {
+      flex: 1;
+      opacity: 1;
+      transition: opacity 0.5s ease;
+
+      & button {
+        background-color: transparent;
+        border: none;
+        font-size: 20px;
+        cursor: pointer;
+        margin-bottom: 20px;
+        transition: transform 0.3s ease, background-color 0.3s ease;
+        text-align: center;
+        padding: 5px 20px;
+        border-radius: 50px;
+
+      }
+      & button:hover{
+          transform: scale(1.1);
+          background-color: rgba(144, 224, 239, 0.3);
+        }
+
+      & img {
+        object-fit: cover;
+        border-radius: 50%;
+        box-shadow: 0 4px 50px rgba(0, 0, 0, 0.8);
+      }
+
+      & h3 {
+        font-size: 30px;
+        color: #000000;
+        margin-bottom: 20px;
+        text-align: center;
+      }
+
+      & p {
+        color: #000;
+        font-size: 20px;
+        text-align: center;
       }
     }
   }
+}
 </style>
